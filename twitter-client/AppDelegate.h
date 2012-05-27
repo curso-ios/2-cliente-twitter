602c3c50d362234d71bef26db63b7376fc564f27
Endika Gutiérrez Salas <endika@endikas-mac-pro.lan>
2012-05-27 19:43:15 +0200

    Initial Commit

---
+++ twitter-client/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  twitter-client
+//
+//  Created by Endika Gutiérrez Salas on 5/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
